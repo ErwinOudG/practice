085f59016f68071fc8e784f29a1e00594e7f8810
Erwin Oudgenoeg <erwin@oudgenoeg.nl>
2017-12-22 15:38:34 +0100

    oop stamp

---
+++ oop/php/classes/cl_stamp.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Postzegel
+ */
+class Stamp
+{
+
+  public $colour = 'blue';
+  function __construct($colour) {
+    $this->colour = $colour;
+  }
+  function setcolour($Newcolour){
+    $this->colour = $Newcolour;
+  }
+}
+
+ ?>
--- oop/php/ooptest.php
+++ oop/php/ooptest.php
@@ -1,9 +1,12 @@
 <?php
-include_once("classes/cl_postzegel.php");
-$nvph1 = new Postzegel;
-var_dump($nvph1);
-$nvph2 = new Postzegel;
-$nvph2->setKleur("rood");
-var_dump($nvph1);
+include_once("classes/cl_stamp.php");
+$nvph1 = new Stamp("blue");
+print_r($nvph1);
+$nvph2 = new Stamp("green");
+print_r($nvph2);
+$nvph2->setcolour("red");
+print_r($nvph2);
+$nvph3 = new Stamp("orange");
+print_r($nvph3);
 
   ?>
