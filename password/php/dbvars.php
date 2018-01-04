9fb0506a92002393b5e688829605a17af9273eaf
Erwin Oudgenoeg <erwin@oudgenoeg.nl>
2018-01-04 14:46:31 +0100

    gitignore changes

--- password/php/dbvars.php
+++ password/php/dbvars.php
@@ -1,8 +1,10 @@
 <?php
+// default database variables
 $servername = "localhost";
 $dbusername = "u12893p9221_pf";
 $dbpassword = "PortfolioEAO";
 $database = "u12893p9221_portfolio";
 $dbmenuitem = "u12893p9221_portfolio.menuitems";
 $sessiondb = "u12893p9221_portfolio.session";
+$logdb = "u12893p9221_portfolio.log";
 ?>
